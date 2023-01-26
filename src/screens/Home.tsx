692276e8e903d12fec145d0fd1db6acaf72f3212
GabrielTSR <gabrielalvesrocha22@gmail.com>
2023-01-26 10:13:09 -0300

    feat: integration with creation habits modal, and summary list

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,24 +1,58 @@
+import { useState, useEffect } from 'react'
 import { useNavigation } from '@react-navigation/native'
-import { Text, View, ScrollView } from 'react-native'
-
+import { Text, View, ScrollView, Alert } from 'react-native'
+import { api } from '../lib/axios'
 import { HabitDay, DAY_SIZE } from '../components/HabitDay'
 import { Header } from '../components/Header'
+import { Loading } from '../components/Loading'
 import { generateDatesFromYearBeginning } from '../utils/generate-dates-from-year-beginning'
+import dayjs from 'dayjs'
 
-const weekDays = ['SU', 'M', 'TU', 'W', 'TH', 'F', 'SA']
+const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S']
 const datesFromYearStart = generateDatesFromYearBeginning()
 const minimumSummaryDatesSizes = 18 * 5
 const amountOfDaysToFill = minimumSummaryDatesSizes - datesFromYearStart.length
 
+type Summary = Array<{
+    id: string
+    date: string
+    amount: number
+    completed: number
+}>
+
 export function Home() {
+    const [loading, setLoading] = useState<boolean>(true)
+    const [summary, setSummary] = useState<Summary>([])
     const { navigate } = useNavigation()
+
+    async function fetchData() {
+        try {
+            setLoading(true)
+            const response = await api.get('summary')
+            setSummary(response.data)
+        } catch (e) {
+            console.error(e)
+            Alert.alert('Ops', 'Não foi possível carregar o sumário de hábitos')
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    useEffect(() => {
+        fetchData()
+    }, [])
+
+    if (loading) {
+        return <Loading />
+    }
+
     return (
         <View className="flex-1 bg-background px-8 pt-16">
             <Header />
             <View className="flex-row mt-6 mb-2">
                 {weekDays.map((weekDay, i) => (
                     <Text
-                        key={weekDay}
+                        key={`${weekDay}${i}`}
                         className="text-zinc-400 text-xl font-bold text-center mx-1"
                         style={{ width: DAY_SIZE }}
                     >
@@ -31,14 +65,24 @@ export function Home() {
                 contentContainerStyle={{ paddingBottom: 100 }}
             >
                 <View className="flex-row flex-wrap">
-                    {datesFromYearStart.map((date) => (
-                        <HabitDay
-                            key={date.toISOString()}
-                            onPress={() =>
-                                navigate('habit', { date: date.toISOString() })
-                            }
-                        />
-                    ))}
+                    {datesFromYearStart.map((date) => {
+                        const dayWithHabits = summary.find((day) => {
+                            return dayjs(date).isSame(day.date, 'day')
+                        })
+                        return (
+                            <HabitDay
+                                key={date.toISOString()}
+                                date={date}
+                                amountOfHabits={dayWithHabits?.amount}
+                                amountCompleted={dayWithHabits?.completed}
+                                onPress={() =>
+                                    navigate('habit', {
+                                        date: date.toISOString(),
+                                    })
+                                }
+                            />
+                        )
+                    })}
 
                     {amountOfDaysToFill > 0 &&
                         Array.from({ length: amountOfDaysToFill }).map(
--- src/screens/New.tsx
+++ src/screens/New.tsx
@@ -5,23 +5,26 @@ import {
     Text,
     TextInput,
     TouchableOpacity,
+    Alert,
 } from 'react-native'
 import { Feather } from '@expo/vector-icons'
 import { BackButton } from '../components/BackButton'
 import { Checkbox } from '../components/Checkbox'
 import colors from 'tailwindcss/colors'
+import { api } from '../lib/axios'
 
 const availableWeekDays = [
-    'Sunday',
-    'Monday',
-    'Tuesday',
-    'Wednesday',
-    'Thursday',
-    'Friday',
-    'Saturday',
+    'Domingo',
+    'Segunda-feira',
+    'Terça-feira',
+    'Quarta-feira',
+    'Quinta-feira',
+    'Sexta-feira',
+    'Sábado',
 ]
 
 export function New() {
+    const [title, setTitle] = useState<string>('')
     const [weekDays, setWeekDays] = useState<number[]>([])
     function handleToggleWeekDay(weekDayIndex: number) {
         if (weekDays.includes(weekDayIndex)) {
@@ -32,6 +35,26 @@ export function New() {
             setWeekDays((prevState) => [...prevState, weekDayIndex])
         }
     }
+
+    async function handleCreateNewHabit() {
+        try {
+            if (!title.trim() || weekDays.length === 0) {
+                return Alert.alert(
+                    'Novo Hábito',
+                    'Informe o nome do hábito, e escolha a periodicidade.'
+                )
+            }
+            await api.post('habits', {
+                title,
+                weekDays,
+            })
+            setTitle('')
+            setWeekDays([])
+            Alert.alert('Novo hábito', 'Hábito criado com sucesso!')
+        } catch (error) {
+            console.error(error)
+        }
+    }
     return (
         <View className="flex-1 bg-background px-8 pt-16">
             <ScrollView
@@ -40,18 +63,20 @@ export function New() {
             >
                 <BackButton />
                 <Text className="mt-6 text-white font-extrabold text-3xl">
-                    Create habit
+                    Criar hábito
                 </Text>
                 <Text className="mt-6 text-white font-semibold text-base">
-                    What is your commitment?
+                    Qual é seu compromisso?
                 </Text>
                 <TextInput
                     className="h-12 pl-4 rounded-lg mt-3 bg-zinc-800 text-white focus:border-2 border-green-600"
-                    placeholder="Exercise, sleep well, etc..."
+                    placeholder="Ex: Exercícios, dormir, e etc..."
                     placeholderTextColor={colors.zinc[400]}
+                    onChangeText={(text) => setTitle(text)}
+                    value={title}
                 />
                 <Text className="mt-4 mb-3 text-white font-semibold text-base">
-                    What is the recurrence?
+                    Qual a recorrência?
                 </Text>
                 {availableWeekDays.map((weekDay, index) => (
                     <Checkbox
@@ -64,9 +89,12 @@ export function New() {
                 <TouchableOpacity
                     className="w-full h-14 flex-row items-center justify-center bg-green-600 rounded-md mt-6"
                     activeOpacity={0.7}
+                    onPress={handleCreateNewHabit}
                 >
                     <Feather name="check" size={20} color={colors.white} />
-                    <Text className="font-semibold text-base text-white ml-2"></Text>
+                    <Text className="font-semibold text-base text-white ml-2">
+                        Confirmar
+                    </Text>
                 </TouchableOpacity>
             </ScrollView>
         </View>
